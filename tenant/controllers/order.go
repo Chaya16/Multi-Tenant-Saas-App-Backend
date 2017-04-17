aedc07476fbc626c96fc31b85b11414c6554c272
Chaya16 <chaya.malik89@gmail.com>
2017-04-16 17:05:43 -0700

    PUT and DELETE APIs

--- tenant/controllers/order.go
+++ tenant/controllers/order.go
@@ -9,7 +9,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/satori/go.uuid"
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 // OrderController represents the controller for operating on the Order resource
@@ -87,21 +86,29 @@ func (oc OrderController) GetOrder(w http.ResponseWriter, r *http.Request) {
 }
 
 //Delete Order deletes the order with specified order id
-func (oc OrderController) DeleteUser(w http.ResponseWriter, r *http.Request) {
+func (oc OrderController) DeleteOrder(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
-	orderId := vars["orderId"]
+	orderId := vars["id"]
 
 	// Verify id is ObjectId, otherwise bail
-	if !bson.IsObjectIdHex(orderId) {
+	/*if !bson.IsObjectIdHex(orderId) {
+		w.WriteHeader(404)
+		return
+	}*/
+
+	o := models.Order{}
+
+	// Fetch order
+	if err := oc.session.DB("test").C("Order").FindId(orderId).One(&o); err != nil {
 		w.WriteHeader(404)
 		return
 	}
 
 	// Grab id
-	oid := bson.ObjectIdHex(orderId)
+	//oid := bson.ObjectIdHex(orderId)
 	// Remove from database
-	if err := oc.session.DB("test").C("Order").RemoveId(oid); err != nil {
+	if err := oc.session.DB("test").C("Order").RemoveId(orderId); err != nil {
 		fmt.Println("Could not find order - %s to delete", orderId)
 		w.WriteHeader(404)
 		return
@@ -109,27 +116,54 @@ func (oc OrderController) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(204)
 }
 
-//Update Order updates the order with specified deails that come in the json format
+//Update Order updates the order with specified details
 func (oc OrderController) UpdateOrder(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
-	orderId := vars["orderId"]
+	orderId := vars["id"]
 
-	// Verify id is ObjectId, otherwise bail
-	if !bson.IsObjectIdHex(orderId) {
+	o := models.Order{}
+
+	// Fetch order
+	json.NewDecoder(r.Body).Decode(&o)
+
+	if err := oc.session.DB("test").C("Order").UpdateId(orderId, &o); err != nil {
 		w.WriteHeader(404)
 		return
 	}
+
+	fmt.Println("Order Updated:", orderId)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(o)
+
+}
+
+// GetOrders retrieves all the orders
+func (oc OrderController) GetOrders(w http.ResponseWriter, r *http.Request) {
 
 	// Grab id
-	oid := bson.ObjectIdHex(orderId)
-	// Remove from database
-	if err := oc.session.DB("test").C("Order").RemoveId(oid); err != nil {
-		fmt.Println("Could not find order - %s to delete", orderId)
+	//vars := mux.Vars(r)
+	//orderId := vars["id"]
+
+	//o := models.Order{}
+	var orders []models.Order
+
+	// Fetch order
+	if err := oc.session.DB("test").C("Order").Find(nil).All(&orders); err != nil {
 		w.WriteHeader(404)
 		return
 	}
-	w.WriteHeader(204)
+
+	// Marshal provided interface into JSON structure
+	//uj, _ := json.Marshal(u)
+
+	// Write content-type, statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(&orders)
+
+	//fmt.Fprintf(w, "%s", uj)
 }
 
 //ping resource function
